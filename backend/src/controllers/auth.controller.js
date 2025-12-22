8a2dc46d86cc8f548f491fe0bf1c116082fad274
Shivam Verma <imshivamv8@gmail.com>
2025-12-22 16:01:01 +0530

    refactored signup code

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -31,8 +31,8 @@ export const signup = async (req, res) => {
     const newUser = new User({ fullName, email, password: hashedPassword });
 
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
 
       return res.status(201).json({
         _id: newUser._id,
